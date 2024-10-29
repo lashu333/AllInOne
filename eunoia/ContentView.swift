15940e7e79fa149e1203148d93172a3d56f9bdbf
lashu333 <140723330+lashu333@users.noreply.github.com>
2024-10-29 14:48:18 +0400

    Initial Commit

---
+++ eunoia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  eunoia
+//
+//  Created by Lasha Tavberidze on 29.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ eunoia/eunoiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  eunoiaApp.swift
+//  eunoia
+//
+//  Created by Lasha Tavberidze on 29.10.24.
+//
+
+import SwiftUI
+
+@main
+struct eunoiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
